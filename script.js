a6d6c2a97ee86e5466fdb7b8656d61598bef3ada
Adnan Waqas <146300388+Code-adnan@users.noreply.github.com>
2023-09-28 14:10:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var con = document.querySelector("#container")
+var love = document.querySelector("i")
+
+con.addEventListener("dblclick", function(){
+    love.style.transform = "translate(-50%, -50%) scale(1)"
+    
+    setTimeout(function(){
+        love.style.transform = "translate(-50%, -50%) scale(0)"
+        love.style.color = "red"
+    },3000);
+})
+
+
